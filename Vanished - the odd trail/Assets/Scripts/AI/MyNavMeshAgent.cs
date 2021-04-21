72c9eb8b1265104a94b2c0e53f00469350eba207
Telmo Francisco <otelmofrancisco@gmail.com>
2021-04-21 20:27:15 +0100

    Deer wander
    Deer only state is wandering

---
+++ Vanished - the odd trail/Assets/Scripts/AI/MyNavMeshAgent.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MyNavMeshAgent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    //public Transform[] waypoints;
+    private int currentWaypoint;
+    public Transform target;
+    public float wanderRadius = 10;
+    public float wanderTimer = 5;
+    private Transform finalPoint;
+    
+    private float timer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //GoToNextWaypoint();
+        timer = wanderTimer;
+    }
+
+    /*public void GoToNextWaypoint()
+    {
+        currentWaypoint = Random.Range(0, waypoints.Length);
+        agent.SetDestination(waypoints[currentWaypoint].position);
+    }*/
+
+    public bool IsAtDestination()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if ((!agent.hasPath) || (agent.velocity.sqrMagnitude == 0))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void GoToTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    public void StopAgent()
+    {
+        agent.isStopped = true;
+        agent.ResetPath();
+    }
+
+    public void MoveAgent(Vector3 offSet)
+    {
+        agent.Move(offSet);
+    }
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
