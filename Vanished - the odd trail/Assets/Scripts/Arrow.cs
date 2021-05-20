4b29ed1989b362f671d1658b864ac22f4b949301
DavidCosta-dev <davidiade2019@gmail.com>
2021-05-20 11:11:18 +0100

    ui, locker

--- Vanished - the odd trail/Assets/Scripts/Arrow.cs
+++ Vanished - the odd trail/Assets/Scripts/Arrow.cs
@@ -8,8 +8,8 @@ public class Arrow : MonoBehaviour
     private bool hasHit = false;
     private bool isActive = false;
 
-    public GameObject hud;
-    public GameObject bow;
+    public HUD hud;
+    private Bow bow;
     public Camera playerCamera;
 
 
@@ -18,8 +18,8 @@ public class Arrow : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         transform.rotation = Quaternion.LookRotation(rb.velocity);
-        hud = GameObject.FindWithTag("HUD");
-        bow = GameObject.FindWithTag("Bow");
+        hud = GameObject.FindWithTag("HUD").GetComponent<HUD>();
+        bow = GameObject.FindWithTag("Bow").GetComponent<Bow>();
         playerCamera = Camera.main;
     }
 
@@ -29,7 +29,7 @@ public class Arrow : MonoBehaviour
         if (!hasHit)
         {
             transform.rotation = Quaternion.LookRotation(rb.velocity);
-            hud.GetComponent<HUD>().CloseMessagePanel();
+            hud.CloseMessagePanel();
         }
 
         if (isActive)
@@ -54,14 +54,18 @@ public class Arrow : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             isActive = true;
+<<<<<<< Updated upstream
             hud.GetComponent<HUD>().OpenMessagePanel("Press F to pickup arrow");
+=======
+            hud.OpenMessagePanel("Press F to pick up arrow");
+>>>>>>> Stashed changes
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
         isActive = false;
-        hud.GetComponent<HUD>().CloseMessagePanel();
+        hud.CloseMessagePanel();
     }
 
     private void Stick()
@@ -73,8 +77,8 @@ public class Arrow : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            bow.GetComponent<Bow>().arrowCount++;
-            hud.GetComponent<HUD>().CloseMessagePanel();
+            bow.arrowCount++;
+            hud.CloseMessagePanel();
             Destroy(gameObject);
         }
     }
