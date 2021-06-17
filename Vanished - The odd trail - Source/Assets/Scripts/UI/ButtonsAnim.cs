8385a377c25acdf93018c23fe0635d47ab3e4b42
DavidCosta-dev <davidiade2019@gmail.com>
2021-06-17 01:51:38 +0100

    more UI

---
+++ Vanished - The odd trail - Source/Assets/Scripts/UI/ButtonsAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void HoverEnter()
+    {
+        anim.SetBool("Hover", true);
+    }
+
+    public void HoverExit()
+    {
+        anim.SetBool("Hover", false);
+    }
+}
